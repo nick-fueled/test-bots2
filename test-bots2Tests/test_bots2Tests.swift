2d2fb0655d4cd624d605b530457acab384e7c1ad
Nick McConnell <ngmcconnell@yahoo.com>
2015-09-10 23:07:15 -0500

    Initial Commit

---
+++ test-bots2Tests/test_bots2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  test_bots2Tests.swift
+//  test-bots2Tests
+//
+//  Created by Nick McConnell on 9/10/15.
+//  Copyright © 2015 Nick McConnell. All rights reserved.
+//
+
+import XCTest
+@testable import test_bots2
+
+class test_bots2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
